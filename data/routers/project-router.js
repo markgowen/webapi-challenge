3c81e87664225cd4d6013e62b9bbc0fd5529fbaa
Mark Gowen <mark.gowen78@gmail.com>
2019-11-01 08:51:28 -0700

    created POST request
    created POST request for adding a new project

--- data/routers/project-router.js
+++ data/routers/project-router.js
@@ -1,19 +1,33 @@
 const express = require('express');
 
-const projectDb = require('../helpers/projectModel')
+const projectDb = require('../helpers/projectModel');
 
 const router = express.Router();
 
 // GET request for all projects
 router.get('/', (req, res) => {
-    projectDb.get()
-        .then(projects => {
-            res.status(200).json(projects);
-        })
-        .catch(err => {
-            console.log(err);
-            res.status(500).json({ error: 'The projects could not be retrieved.' })
-        });
+  projectDb
+    .get()
+    .then(projects => {
+      res.status(200).json(projects);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json({ error: 'The projects could not be retrieved.' });
+    });
+});
+
+// POST request to add a project
+router.post('/', (req, res) => {
+  projectDb
+    .insert(req.body)
+    .then(project => {
+      res.status(201).json(project);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json({ message: 'Error adding project' });
+    });
 });
 
 module.exports = router;
