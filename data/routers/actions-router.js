5a4cf3ede18baeb0233e4ddf01b5c2a5c6167b8e
Mark Gowen <mark.gowen78@gmail.com>
2019-11-01 09:41:06 -0700

    created custom middleware
    created validateProjectId to validate that a project exists

--- data/routers/actions-router.js
+++ data/routers/actions-router.js
@@ -7,8 +7,13 @@ const router = express.Router();
 
 
 // Middleware
-// function validateProjectId (req, res, next) {
-//     if (req.params.id)
-// }
+function validateProjectId (req, res, next) {
+    if (req.params.id) {
+        return;
+    } else {
+        res.status(404).json({ message: 'invalid project id' })
+    }
+    next();
+}
 
 module.exports = router;
